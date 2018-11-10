42708a09d29e6e553c96e8cc45b51dedaa98301c
Jeferson Natanael <je_natanael@hotmail.com>
2018-11-09 22:00:41 -0200

    teste

--- Lan-House/Lan-House/Program.cs
+++ Lan-House/Lan-House/Program.cs
@@ -16,7 +16,7 @@ namespace Lan_House
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Inicio());
         }
     }
 }
